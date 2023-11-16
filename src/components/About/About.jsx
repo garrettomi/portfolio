131338a863500ae7cf8298c3ab65e0e005506a84
garrettomi <omigarrett@gmail.com>
2023-11-16 22:00:52 +0900

    Feat: Implement basic header

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div className='content'>
+            <h1>About Me</h1>
+            <p>Information about me....</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <button className='tab-button'>About</button>
+            <button className='tab-button'>Projects</button>
+            <button className='tab-button'>Contact</button>
+        </div>
+    );
+};
+
+export default Header;
