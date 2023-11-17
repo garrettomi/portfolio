2a73c6e793d5794f959af38882cce1e495f71a44
garrettomi <omigarrett@gmail.com>
2023-11-17 19:49:44 +0900

    Update: Footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import logoGreenSmall from '../../assets/logo-green-small-1x.png'
 import logoGreenSmall2 from '../../assets/logo-green-small-2x.png'
 import logoGreen from '../../assets/logo-green-1x.png'
 import logoGreen2 from '../../assets/logo-green-2x.png'
+import githubLogo from '../../assets/github.png';
+import linkedinLogo from '../../assets/linkedin.png';
 
 
 const Footer = () => {
@@ -18,17 +20,14 @@ const Footer = () => {
             <div className="col-1-of-2">
               <div className="footer__navigation">
                 <ul className="footer__list">
-                  <li className="footer__item"><a href="#" className="footer__link">Company</a></li>
-                  <li className="footer__item"><a href="#" className="footer__link">Contact us</a></li>
-                  <li className="footer__item"><a href="#" className="footer__link">Carrers</a></li>
-                  <li className="footer__item"><a href="#" className="footer__link">Privacy policy</a></li>
-                  <li className="footer__item"><a href="#" className="footer__link">Terms</a></li>
+                  <li className="footer__item"><a href="https://www.linkedin.com/in/garrett-omi/" className="footer__link">LinkedIn</a></li>
+                  <li className="footer__item"><a href="https://github.com/garrettomi" className="footer__link">Github</a></li>
                 </ul>
               </div>
             </div>
             <div className="col-1-of-2">
               <p className="footer__copyright">
-                Some text
+                omigarrett@gmail.com
               </p>
             </div>
           </div>
