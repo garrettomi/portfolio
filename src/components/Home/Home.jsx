eca1c7282fd373c3f36b9bd633bdf51c3eca5281
garrettomi <omigarrett@gmail.com>
2023-11-17 19:37:38 +0900

    Update: Add rounding to badges and remove text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -51,41 +51,46 @@ const Home = () => {
             </div>
           </section>
           <section className="section-features">
+          <div className="u-center-text u-margin-bottom-big">
+              <h2 className="heading-secondary">
+                Tech Stack
+              </h2>
+            </div>
             <div className="row">
               <div className="col-1-of-4">
                 <div className="feature-box">
                   <img src={reactLogo} alt='React Logo' className="feature-box__icon" />
-                  <h3 className="heading-tertiary u-margin-bottom-small">React</h3>
-                  <p className="feature-box__text">
+                  {/* <h3 className="heading-tertiary u-margin-bottom-small">React</h3> */}
+                  {/* <p className="feature-box__text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                  </p>
+                  </p> */}
                 </div>
               </div>
               <div className="col-1-of-4">
                 <div className="feature-box">
                 <img src={pythonLogo} alt='Python Logo' className="feature-box__icon" />
-                  <h3 className="heading-tertiary u-margin-bottom-small">Python</h3>
-                  <p className="feature-box__text">
+                  {/* <h3 className="heading-tertiary u-margin-bottom-small">Python</h3> */}
+                  {/* <p className="feature-box__text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                  </p>
+                  </p> */}
                 </div>
               </div>
               <div className="col-1-of-4">
                 <div className="feature-box">
                 <img src={djangoLogo} alt='Django Logo' className="feature-box__icon" />
-                  <h3 className="heading-tertiary u-margin-bottom-small">Django</h3>
-                  <p className="feature-box__text">
+                  {/* <h3 className="heading-tertiary u-margin-bottom-small">Django</h3> */}
+                  {/* <p className="feature-box__text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                  </p>
+                  </p> */}
                 </div>
               </div>
               <div className="col-1-of-4">
                 <div className="feature-box">
                 <img src={nodejsLogo} alt='NodeJS Logo' className="feature-box__icon" />
-                  <h3 className="heading-tertiary u-margin-bottom-small">Node.js</h3>
-                  <p className="feature-box__text">
+                  {/* <h3 className="heading-tertiary u-margin-bottom-small">Node.js</h3> */}
+                  {/* <p className="feature-box__text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                  </p>
+                  </p> */}
                 </div>
               </div>
             </div>
