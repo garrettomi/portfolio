e70e2b5f90e11151e0b9fa88719309654121dcf5
garrettomi <omigarrett@gmail.com>
2023-11-17 17:45:57 +0900

    Feat: Add linkedin

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,5 @@
-import githubLogo from '../../assets/github.png'
+import githubLogo from '../../assets/github.png';
+import linkedinLogo from '../../assets/linkedin.png';
 
 const Navigation = () => {
     
@@ -14,7 +15,7 @@ const Navigation = () => {
 
             <nav class="navigation__nav">
                 <ul class="navigation__list">
-                    <li class="navigation__item"><a href="#" class="navigation__link"><span>01</span>LinkedIn</a></li>
+                    <li class="navigation__item"><a href="https://www.linkedin.com/in/garrett-omi/" class="navigation__link" target='_blank'><img src={linkedinLogo} alt='LinkedIn Logo' />LinkedIn</a></li>
                     <li class="navigation__item"><a href="https://github.com/garrettomi" class="navigation__link" target='_blank'><img src={githubLogo} alt='Github Logo' />Github</a></li>
                     <li class="navigation__item"><a href="#" class="navigation__link"><span>03</span>Gmail</a></li>
                     </ul>
