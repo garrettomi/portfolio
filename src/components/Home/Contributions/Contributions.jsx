e87b29282a3cc065e0a7f6eaae07b4e846f888ee
garrettomi <omigarrett@gmail.com>
2023-11-18 20:44:33 +0900

    Update: Remove commented out code from Contributions

--- src/components/Home/Contributions/Contributions.jsx
+++ src/components/Home/Contributions/Contributions.jsx
@@ -22,25 +22,6 @@ const Contributions = () => {
               <a href="#" className="btn btn--green">Back to the top</a>
             </div>
             </div>
-            {/* <div className="row">
-              <div className="story">
-                <figure className="story__shape">
-                  <img src={discoverNikkei} alt="Person on a tour" className="story__img" />
-                  <figcaption className="story__caption">Jack Wilson</figcaption>
-                </figure>
-                <div className="story__text">
-                  <h3 className="heading-tertiary u-margin-bottom-small">WOW! My life is completely different now</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                    ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero
-                    repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.
-                  </p>
-                </div>
-              </div>
-            </div> */}
-            {/* <div className="u-center-text u-margin-top-huge">
-              <a href="#" className="btn-text">Read all stories →</a>
-            </div> */}
           </section>
     )
 }
