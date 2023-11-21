a4fd937f5f2b50f61f0ed0bc88ce76a876e6c26f
garrettomi <omigarrett@gmail.com>
2023-11-21 18:42:51 +0900

    Update: Implement simultaneous typing effect for main and sub animations

--- src/components/Header/Heading/Heading.jsx
+++ src/components/Header/Heading/Heading.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import Typewriter from "../Typewriter/Typewriter";
 
 const Heading = ({main, sub}) => {
+    const [isMainComplete, setIsMainComplete] = useState(false);
 
     return (
         <h1 className='heading-primary'>
             {/* <span className='heading-primary--main'>{main}</span>
             <span className='heading-primary--sub'>{sub}</span> */}
-            <Typewriter className='heading-primary--main' text={main} />
-            <Typewriter className='heading-primary--sub' text={sub} />
+            <Typewriter className='heading-primary--main' text={main} onComplete={() => setIsMainComplete(true)} />
+           {isMainComplete && <Typewriter className='heading-primary--sub' text={sub} />}
         </h1>
     );
 };
--- src/components/Header/Typewriter/Typewriter.jsx
+++ src/components/Header/Typewriter/Typewriter.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const Typewriter = ({text, className}) => {
+const Typewriter = ({text, className, onComplete}) => {
     const [index, setIndex] = useState(0);
     const [displayText, setDisplayText] = useState('');
 
@@ -8,10 +8,13 @@ const Typewriter = ({text, className}) => {
         const timer = setTimeout(() => {
             setDisplayText(text.slice(0, index + 1));
             setIndex(index + 1);
-        }, 100);
-
+            if (index + 1 === text.length) {
+                onComplete && onComplete();
+            }
+        }, 150);
+ 
         return () => clearTimeout(timer);
-    }, [index, text]);
+    }, [index, text, onComplete]);
 
     return <span className={className}>{displayText}</span>
 };
