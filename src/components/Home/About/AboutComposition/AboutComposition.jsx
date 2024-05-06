d5c366e415ee9a2598ba314d3be2729e4e4f4a3d
garrettomi <omigarrett@gmail.com>
2024-05-06 19:45:57 +0900

    Update: Remove extra about me photos

--- src/components/Home/About/AboutComposition/AboutComposition.jsx
+++ src/components/Home/About/AboutComposition/AboutComposition.jsx
@@ -1,17 +1,11 @@
 import AboutImage from "../AboutImage/AboutImage";
 
-import misterGarrett from '../../../../assets/mister-garrett.jpeg';
-import misterGarrettLarge from '../../../../assets/mister-garrett-large.jpg';
-import wedding from '../../../../assets/wedding.jpeg';
-import weddingLarge from '../../../../assets/wedding-large.jpg';
 import headshotPhoto from '../../../../assets/headshot-photo.png'
 
 const AboutComposition = () => {
 
     return (
         <div className="composition">
-            <AboutImage srcSet={`${misterGarrett} 300w, ${misterGarrettLarge} 1000w`} sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px" alt="Student Drawing of Me" className="composition__photo composition__photo--p1" />
-            <AboutImage srcSet={`${wedding} 300w, ${weddingLarge} 1000w`} sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px" alt="Wedding Photo" className="composition__photo composition__photo--p2" />
             <AboutImage srcSet={`${headshotPhoto} 300w, ${headshotPhoto} 1000w`} sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px" alt="Coding at work with my son" className="composition__photo composition__photo--p3" />
         </div>
     )
