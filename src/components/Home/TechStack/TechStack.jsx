eff61cc724edfbe9301d11c5630c423a1c201e91
garrettomi <omigarrett@gmail.com>
2026-05-28 11:29:15 +0900

    chore: update tech stack

--- src/components/Home/TechStack/TechStack.jsx
+++ src/components/Home/TechStack/TechStack.jsx
@@ -1,34 +1,38 @@
-import Badges from './Badges/Badges';
+import Badges from "./Badges/Badges";
 
-import reactLogo from '../../../assets/react-icon.png';
-import pythonLogo from '../../../assets/python-logo.png';
-import djangoLogo from '../../../assets/django-logo.png';
-import nodejsLogo from '../../../assets/nodejs-logo.png';
+import reactLogo from "../../../assets/react-icon.png";
+import nextJSLogo from "../../../assets/nextjs-logo.webp";
+import typescriptLogo from "../../../assets/typescript-logo.webp";
+import tailwindLogo from "../../../assets/tailwind-logo.png";
+import nodejsLogo from "../../../assets/nodejs-logo.png";
+import graphqlLogo from "../../../assets/graphql-logo.webp";
+import postgresqlLogo from "../../../assets/postgresql-logo.webp";
 
 const TechStack = () => {
-    const allBadges = [
-      { logo: reactLogo, altText: 'React Logo' },
-      { logo: pythonLogo, altText: 'Python Logo' },
-      { logo: djangoLogo, altText: 'Django Logo' },
-      { logo: nodejsLogo, altText: 'NodeJS Logo' },
-    ]
+  const allBadges = [
+    { logo: reactLogo, altText: "React Logo" },
+    { logo: nextJSLogo, altText: "NextJS Logo" },
+    { logo: typescriptLogo, altText: "TypeScript Logo" },
+    { logo: nodejsLogo, altText: "NodeJS Logo" },
+    { logo: tailwindLogo, altText: "Tailwind Logo" },
+    { logo: graphqlLogo, altText: "GraphQL Logo" },
+    { logo: postgresqlLogo, altText: "PostgreSQL Logo" },
+  ];
 
-    return (
-        <section className="section-tech-stack">
-        <div className="u-center-text u-margin-bottom-big">
-            <h2 className="heading-secondary">
-              Tech Stack
-            </h2>
+  return (
+    <section className="section-tech-stack">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">Tech Stack</h2>
+      </div>
+      <div className="row">
+        {allBadges.map((badge, index) => (
+          <div className="col-1-of-4" key={index}>
+            <Badges logo={badge.logo} altText={badge.altText} />
           </div>
-          <div className="row">
-            {allBadges.map((badge, index) => (
-              <div className='col-1-of-4' key={index}>
-                <Badges logo={badge.logo} altText={badge.altText} />
-                </div>
-            ))}
-          </div>
-        </section>
-    )
-}
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default TechStack;
